e5ecff922a3adade4e0744b01b1e17b8800915b0
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-18 04:06:57 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,7 +15,7 @@ class EmailAddressParser
 
   def parse
     binding.pry
-    email_addresses.split(/\,|\s/).delete_if(&:blank?).uniq
+    email_addresses.split(/\,|\s/).uniq
     #remove duplicate emails and " "
   end
 
